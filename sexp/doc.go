dbc577de5e4ad78e04b9225b3d97a66bb1334291
Dan Kortschak <dan@kortschak.io>
2020-09-11 13:45:49 +0930

    sexp: add warning for use of package
    See https://stat.ethz.ch/pipermail/r-devel/2020-September/079906.html

--- sexp/doc.go
+++ sexp/doc.go
@@ -5,6 +5,14 @@
 // Package sexp provides an API for interacting with R SEXP values.
 // Except where noted, values are allocated by the R runtime.
 //
+// Here be dragons
+//
+// The API provided by the sexp package only minimally interacts with
+// the R runtime, so deviations from behavior expect by the R runtime
+// are possible. This package should be used with caution and client
+// packages should ensure correct behavior with adequate testing from
+// the R side.
+//
 // Building the sexp package requires having CGO_LDFLAGS_ALLOW="-Wl,-Bsymbolic-functions"
 // and installations of R and pkg-config.
 package sexp
